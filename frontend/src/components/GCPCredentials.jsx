09ecbb0ff7fc54480b6b6ec0d5c85c9f8990ad7a
Anishthimmaiahgari <anish.thimmaiahgari@bilvantis.io>
2025-03-26 11:22:44 +0530

    Update GCPCredentials.jsx

--- frontend/src/components/GCPCredentials.jsx
+++ frontend/src/components/GCPCredentials.jsx
@@ -107,7 +107,7 @@ function GCPCredentials() {
           <div className="flex items-center">
             {/* Bilvantis Logo */}
             <img 
-              src="https://bilvantis.io/wp-content/uploads/2022/12/Bilvantis-logo-png.jpg" 
+              src="https://th.bing.com/th/id/OIP.PQzWQzdJEXwoGdeuUfsnUAHaCL?rs=1&pid=ImgDetMain" 
               alt="Bilvantis Logo" 
               className="h-8 mr-3 rounded"
             />
